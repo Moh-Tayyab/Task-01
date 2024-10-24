e5e62bc610b9c4872446462be4dc3d245200b121
Muhammad-Tayyab <m.tayyab1263@gmail.com>
2024-10-24 14:19:58 +0500

    PWM Update...

---
+++ class_02.py
@@ -0,0 +1,32 @@
+#[Variables]
+name = "Alice";
+greet = 'Happy to Start Python Programming Language...'
+
+# Print statement
+print("Hello, " + name + "!");
+print(greet);
+
+#[Data type]
+
+#Integer
+int_num = 5;
+print(int_num);
+
+#Float
+float_num = 5.2;
+print(float_num);
+
+#String
+str_var = "Hello, World!";
+print(str_var);
+
+#Boolean
+bool_var = True;
+print(bool_var);
+
+#Input
+user_input = input("Please enter your name: ");
+
+#Output
+print("Hello, " + user_input + "!");
+
---
+++ task00.py
@@ -0,0 +1,28 @@
+#Input
+name= input("Enter your name: ");
+
+age = input("Enter your age: ");
+
+email = input("Enter your email:  ");
+
+phoneNum = input("Enter your phone number:  ");
+
+greet = input("Enter a greeting message: "    );
+
+# Print the name and age
+print("Hello," + name + "you are" + age + "year old!");
+
+#Print the email
+print("Your email is: " + email);
+
+# Print the phone number
+print("Your phone number is: " + phoneNum);
+
+# Print the greeting message
+print(greet + "!");
+
+#use f-string for the greeting
+#print(f"Your name is {name} and your age is {age}.");
+
+# Use f-string for the greeting
+#print(f"Greetings, {name}! {greet}");
